9c78dfe8feaad0b6a76b771cd3eb5bde29831111
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-09-13 22:22:10 +0300

    [feat #160406085] add endpoint to login user

--- app/views/views.py
+++ app/views/views.py
@@ -5,18 +5,25 @@ import psycopg2
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, jwt_required
+from flask_jwt_extended import create_access_token, get_jwt_identity
 
 
 from instance.config import app_config
 from app.models.models import Question, Answer, User
 from app.utilities import validate_question, validate_answer, validate_signup
 from app.utilities import validate_username, validate_email, validate_password
+from app.utilities import validate_login
 from instance.config import conn
 
 config_name = os.getenv('APP_SETTINGS')
 app = Flask(__name__, instance_relative_config=True)
+app.config['JWT_SECRET_KEY'] = os.getenv('SECRET')
 app.config.from_object(app_config[config_name])
 app.url_map.strict_slashes = False
+
+
+jwt = JWTManager(app)
 api = Api(app)
 
 
@@ -111,9 +118,57 @@ class UserSignup(Resource):
                 return "Failed to add user. {}".format(error)
 
 
+class UserLogin(Resource):
+    """Class for user sign in"""
+
+    def post(self):
+        """
+        login user
+        """
+        user_details = request.get_json()
+
+        # Validate login details provided
+        if validate_login(user_details):
+            return validate_login(user_details)
+
+        email = user_details["email"]
+        password = user_details["password"]
+
+        # Check and log in user
+        try:
+            users = User.get_users(cursor)
+            if users:
+                for user in users:
+                    check_pass = check_password_hash(
+                        user["password"], password)
+                    if email != user["email"] or not check_pass:
+                        message = "incorrect username or password"
+                        response = jsonify({"login failed": message})
+                        response.status_code = 401
+                        return response
+
+                    # Grab user info for later user
+                    username = user["username"]
+                    user_id = user["user_id"]
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            if(conn):
+                return "Failed to get users. {}".format(error)
+
+        # create access token with username and id
+        identity_dict = {'user_id': user_id, 'username': username}
+        access_token = create_access_token(identity=identity_dict)
+
+        # return responses
+        message = "Logged in as {}".format(username)
+        response = jsonify({"success": message, "access_token": access_token})
+        response.status_code = 200
+        return response
+
+
 class QuestionsView(Resource):
     """Resource class for posting and getting all questions """
-
+    @jwt_required
     def post(self):
         """
         Post a question
@@ -131,7 +186,7 @@ class QuestionsView(Resource):
             if questions:
                 for question in questions:
                     if question["title"] == question_details["title"]:
-                        response = jsonify({"Error": "Question already asked"})
+                        response = jsonify({"error": "question already asked"})
                         response.status_code = 400
                         return response
         except (Exception, psycopg2.DatabaseError) as error:
@@ -139,10 +194,11 @@ class QuestionsView(Resource):
                 return "Failed to get questions. {}".format(error)
         # Validation checks passed.
 
+        current_user = get_jwt_identity()
         title = question_details["title"]
         description = question_details["description"]
         date_created = (now.strftime("%d-%m-%Y %H:%M:%S"))
-        user_id = 1
+        user_id = current_user["user_id"]
 
         try:
             question = Question(title, description, date_created, user_id)
@@ -157,6 +213,7 @@ class QuestionsView(Resource):
                 conn.rollback()
                 return "Failed to insert record. {}".format(error)
 
+    @jwt_required
     def get(self):
         """
         Get all questions
@@ -179,7 +236,7 @@ class QuestionView(Resource):
     """
     Class to get one question as well as delete
     """
-
+    @jwt_required
     def get(self, qn_id):
         """
         get a specific question"""
@@ -189,6 +246,7 @@ class QuestionView(Resource):
                 response = jsonify({"error": "question doesn't exist"})
                 response.status_code = 404
                 return response
+
             response = jsonify({"question": question})
             response.status_code = 200
             return response
@@ -197,6 +255,7 @@ class QuestionView(Resource):
             if(conn):
                 return "Failed to fetch question. {}".format(error)
 
+    @jwt_required
     def delete(self, qn_id):
         """
         Delete a question
@@ -221,7 +280,7 @@ class AnswersView(Resource):
     """
     Class for posting of answers
     """
-
+    @jwt_required
     def post(self, qn_id):
         """
         post a question
@@ -234,9 +293,10 @@ class AnswersView(Resource):
             return validate_answer(answer_details)
 
         # post answer
+        current_user = get_jwt_identity()
         description = answer_details["description"]
         date_created = (now.strftime("%d-%m-%Y %H:%M:%S"))
-        user_id = 1
+        user_id = current_user["user_id"]
 
         try:
             question = Question.get_one_question(cursor, qn_id)
@@ -264,3 +324,5 @@ api.add_resource(
     AnswersView, '/stackoverflowlite/api/v1/questions/<int:qn_id>/answers')
 api.add_resource(
     UserSignup, '/stackoverflowlite/api/v1/auth/signup')
+api.add_resource(
+    UserLogin, '/stackoverflowlite/api/v1/auth/login')
