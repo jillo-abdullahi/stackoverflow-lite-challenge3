539e63a81ba65403eea9cefadfd3f84d2f5773ba
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-09-18 09:30:27 +0300

    update with db table creations

--- run.py
+++ run.py
@@ -2,11 +2,15 @@
 import os
 from flask_restful import Api
 from app import create_app
+from migrations import migration
 
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
 
 api = Api(app)
+
+# Create tables
+migration()
 
 
 if __name__ == '__main__':
